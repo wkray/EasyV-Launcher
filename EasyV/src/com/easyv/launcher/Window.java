ebc54732044499cd45fc1fe3ebe3c19d7719b0b7
William <raywilliam1212@yahoo.com>
2015-05-08 15:08:06 -0500

    Basic window

--- EasyV/src/com/easyv/launcher/Window.java
+++ EasyV/src/com/easyv/launcher/Window.java
@@ -1,9 +1,21 @@
 package com.easyv.launcher;
 
-public class Window {
+import java.awt.GridBagLayout;
 
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+	
+	public static final String VERSION = "InDev";
+	
 	public static void main(String[] args) {
-
+		JFrame mainWindow = new JFrame("EasyV Launcher v" + VERSION);
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainWindow.setLayout(new GridBagLayout());
+		mainWindow.setVisible(true);
+		mainWindow.setSize(1000, 600);
+		mainWindow.setResizable(true);
 	}
 
 }
