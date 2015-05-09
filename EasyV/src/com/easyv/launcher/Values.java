1c2183924aee0b51495b06f5a49b6119bcd74309
= <benjamin.mchone@gmail.com>
2015-05-08 23:46:06 -0500

    Added a file with the 'clean install'

---
+++ EasyV/src/com/easyv/launcher/Values.java
@@ -0,0 +1,8 @@
+package com.easyv.launcher;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Values {
+	public static final ArrayList<String> cleanInstall = new ArrayList<String>(Arrays.asList("bink2w64.dll", "commandline.txt", "common.rpf", "d3dcompiler_46.dll", "d3dcsx_46.dll", "GFSDK_ShadowLib.win64.dll", "GFSDK_TXAA.win64.dll", "GFSDK_TXAA_AlphaResolve.win64.dll", "GTA5.exe", "PlayGTAV.exe", "GTAVLauncher.exe", "Installers", "installscript.vdf", "steam_api64.dll", "steam_appid.txt", "update", "x64", "x64a.rpf", "x64b.rpf", "x64c.rpf", "x64d.rpf", "x64e.rpf", "x64f.rpf", "x64g.rpf", "x64h.rpf", "x64i.rpf", "x64j.rpf", "x64k.rpf", "x64l.rpf", "x64m.rpf", "x64n.rpf", "x64o.rpf", "x64p.rpf", "x64q.rpf", "x64r.rpf", "x64s.rpf", "x64t.rpf", "x64u.rpf", "x64v.rpf", "x64w.rpf", "_CommonRedist"));
+}
